186c54d1e366499c1fc7b0934dd52f7c61215283
Paul Bennett <pbennett.uk@gmail.com>
2024-06-15 10:50:29 +0100

    adding a k3s cluster

--- servers/ubuntu.sh
+++ servers/ubuntu.sh
@@ -6,11 +6,12 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 # install qemu
 sudo apt-get install qemu-guest-agent -y && sudo systemctl start qemu-guest-agent && sudo systemctl enable qemu-guest-agent
 
-# install net-tools
-sudo apt install git -y && sudo apt install nano -y
-
 # install oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+# Kubernetes only
+sudo apt install open-iscsi
+sudo ufw disable
 
 # reboot server
 sudo reboot
