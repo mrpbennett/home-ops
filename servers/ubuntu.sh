543dd8525304498040267030d669a599920bf054
Paul Bennett <pbennett.uk@gmail.com>
2024-08-01 12:52:03 +0100

    giving cloudnative a go

--- servers/ubuntu.sh
+++ servers/ubuntu.sh
@@ -9,7 +9,7 @@ sudo systemctl enable open-iscsi --now
 sudo ufw disable
 
 # ubuntu minimal
-sudo apt install curl nano jq vim git ufw -y
+sudo apt install curl nano jq git ufw -y
 
 # install qemu
 sudo apt-get install qemu-guest-agent -y && sudo systemctl start qemu-guest-agent && sudo systemctl enable qemu-guest-agent
