66fc3bacf1fbf903765a34d6c2fcedb605edc4be
Paul Bennett <pbennett.uk@gmail.com>
2024-09-27 13:53:30 +0100

    ...

--- scripts/add-hosts.sh
+++ scripts/add-hosts.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Define the IP address
-IP="192.168.5.58"
+IP="192.168.5.61"
 
 # Define the hostnames in an array
 HOSTNAMES=(
     "longhorn.70ld.home"
     "pgadmin.70ld.home"
     "homepage.70ld.home"
-    "redis-insight.70ld.home"
-    "elastic.70ld.home"
+    "redisinsight.70ld.home"
     "kibana.70ld.home"
-    "logstash.70ld.home"
     "grafana.70ld.home"
     "prometheus.70ld.home"
     "70ld.home"
