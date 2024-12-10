aac0575eedbc1a1744f48b871fde412023777019
Paul Bennett <pbennett.uk@gmail.com>
2024-12-10 12:11:35 +0000

    trying to install pg_cron on the dev cluster

--- scripts/add-hosts.sh
+++ scripts/add-hosts.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Define the IP address
-IP="192.168.5.61"
+IP="192.168.5.69"
 
 # Define the hostnames in an array
 HOSTNAMES=(
     "longhorn.70ld.home"
     "pgadmin.70ld.home"
     "homepage.70ld.home"
-    "redisinsight.70ld.home"
-    "kibana.70ld.home"
     "grafana.70ld.home"
     "prometheus.70ld.home"
-    "70ld.home"
+    "alertmanager.70ld.home"
+    "gatus.70ld.home"
+    "minio-console.70ld.home"
 )
 
 # Loop through the array and append each entry to /etc/hosts
